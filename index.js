976d4a0fc303ca9c7d9f2f95d338d377743eb9bc
Christopher Wright <chrisalanwright@gmail.com>
2025-07-23 22:24:42 -0400

    finished-workshop

---
+++ index.js
@@ -0,0 +1,19 @@
+let code1;
+
+code1 = 7 + 3;
+
+let code2 = 82 / 2 - 2;
+
+let code3 = 79 % 40;
+
+let message = `"The vault has been secured. The combination is:`;
+
+let codeA = code1 + "-" + code2 + "-" + code3;
+
+let codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message);
+
+console.log(codeA);
+
+console.log(codeB);
